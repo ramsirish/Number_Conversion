e3b75f6365d4ba071d97ff87c16b0d394ca2bce3
Tony <32371862+sktonyca@users.noreply.github.com>
2018-07-24 10:44:17 -0400

    Add files via upload

---
+++ Calculator/Page1.aspx
@@ -0,0 +1,45 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Page1.aspx.cs" Inherits="Calculator.Page1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <link rel="stylesheet" type="html/css" href="Page1.css" />
+    <title>Binary to Decimal</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <h2>Number Conversion</h2>
+
+        <p>
+            <asp:TextBox ID="getInput" runat="server" Height="25px" Width="150px"></asp:TextBox>
+            <asp:DropDownList ID="convertFrom" runat="server" style="margin-left: 30px">
+            <asp:ListItem Value ="-1">Convert From ...</asp:ListItem>
+            <asp:ListItem Value = "0">Binary</asp:ListItem>
+            <asp:ListItem Value = "1">Decimal</asp:ListItem>
+            <asp:ListItem Value = "2">Hexadecimal</asp:ListItem>
+            </asp:DropDownList>
+            <asp:DropDownList ID="convertTo" runat="server" style="margin-left: 61px">
+            <asp:ListItem Value ="-1">Convert To ...</asp:ListItem>
+            <asp:ListItem Value = "0">Binary</asp:ListItem>
+            <asp:ListItem Value = "1">Decimal</asp:ListItem>
+            <asp:ListItem Value = "2">Hexadecimal</asp:ListItem>
+            </asp:DropDownList>
+
+        </p>
+
+       
+        <p>
+            <asp:Button ID="enterButton" runat="server" Text="Convert" OnClick="Submit_Click" />
+        </p>
+
+            <asp:TextBox ID="resultBox" runat="server"  Height="25px" Width="150px" ReadOnly="true"></asp:TextBox>
+
+
+
+    </form>
+           
+
+    </body>
+
+</html>
---
+++ Calculator/Page1.aspx.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Calculator
+{
+    public partial class Page1 : System.Web.UI.Page
+    {
+        string fromInput;
+        string finalNum;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+
+        }
+
+        protected void Submit_Click(object sender, EventArgs e)
+        {
+            fromInput = getInput.Text;
+
+            // Input is Binary
+            if(convertFrom.SelectedIndex == 1)
+            {
+                if(convertTo.SelectedIndex == 1)
+                {
+                    resultBox.Text = "The number is already in Binary";
+                }
+
+                // Binary to Decimal
+                else if(convertTo.SelectedIndex == 2)
+                {
+                    finalNum = Convert.ToInt64(fromInput, 2).ToString();
+                    resultBox.Text = finalNum;
+                }
+                // Binary to Hexadecimal
+                else if (convertTo.SelectedIndex == 3)
+                {
+                    finalNum = Convert.ToInt64(fromInput, 2).ToString("X");
+                    resultBox.Text = finalNum;
+
+                }
+            }
+
+            // Input is Decimal
+            if (convertFrom.SelectedIndex == 2)
+            {
+                // Decimal to Binary
+                if (convertTo.SelectedIndex == 1)
+                {
+                    finalNum = Convert.ToString(Convert.ToInt64(fromInput), 2);
+                    resultBox.Text = finalNum;
+                }
+
+                else if (convertTo.SelectedIndex == 2)
+                {
+                    resultBox.Text = "The number is already a Decimal";
+                }
+                // Decimal to Hexadecimal
+                else if (convertTo.SelectedIndex == 3)
+                {
+                    finalNum = Convert.ToInt64(fromInput).ToString("X");
+                    resultBox.Text = finalNum;
+                }
+            }
+
+
+            // Input is Hexadecimal
+            if (convertFrom.SelectedIndex == 3)
+            {
+                // Hexadecimal to Binary
+                if (convertTo.SelectedIndex == 1)
+                {
+                    finalNum = Convert.ToString(Convert.ToInt64(fromInput, 16), 2);
+                    resultBox.Text = finalNum;
+                }
+
+                // Hexadecimal to Decimal
+                else if (convertTo.SelectedIndex == 2)
+                {
+                    finalNum = Convert.ToInt64(fromInput, 16).ToString();
+                    resultBox.Text = finalNum;
+                }
+              
+                else if (convertTo.SelectedIndex == 3)
+                {
+                    resultBox.Text = "The number is already a Hexadecimal";
+                }
+
+            }
+        }
+    }
+}
